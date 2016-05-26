92e5a5179a043569b0d870f97d580f09c5c10827
obligat <duzi1996@qq.com>
2016-05-26 16:57:32 +0800

    second commit

--- src/separators.js
+++ src/separators.js
@@ -1,28 +1,19 @@
 'use strict';
 
 function thousands_separators(num) {
-  var number = num.string().split("");
-  number =parseFloat((number+""));
-  var result="";
-  if(number.indexOf(".")==-1)
-  {
-	  var front=number.split("");
-	  for(var i=0;i<front.length;i++)
-  {
-	  result+=front[i]+((i+1)%3==0&&(i+1)!=front.length?",":"");
-  }
-  return result.split("").join("");
-  }
-  else {
-	  var front=number.split(".")[0].split("");
-	  var back=number.split(".")[1];
-	   for(var i=0;i<front.length;i++)
-  {
-	  result+=front[i]+((i+1)%3==0&&(i+1)!=front.length?",":"");
-  }
-	  return result.split("").join("")+"."+back;
-  }
-  
+    var number = num.toString();
+	
+	var re = /(\d{1,3})(?=(\d{3})+$)/g;
+	if(number.indexOf(".")!=-1)
+	{
+		var sort=number.split(".");
+		var front=sort[0];
+		var back=sort[1];
+		return front.replace(re,"$1,")+"."+back;
+	}
+	else{
+		return number.replace(re,"$1,");
+	}
 }
 
 module.exports = thousands_separators;
